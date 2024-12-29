8523e603213c026d7ddaa7a1440779204dd1bac0
Dani-GT <dani_spain87@hotmail.com>
2024-12-29 22:09:58 +0100

    ajuste final responsive

--- Async_project/components/Header/Header.js
+++ Async_project/components/Header/Header.js
@@ -1,18 +1,22 @@
 import "./Header.css";
 
 const template = () => `
-<h1> Photosplash</h1>
-<input type="text" id="searchInput" placeholder="Ejemplo: gato, perro..." />
-<select id="countInput">
-    <option value="20">20</option>
-    <option value="30">30</option>
-    <option value="50">50</option>
-</select>
-<select id="orderBy">
-    <option value="relevant">Relevante</option>
-    <option value="latest">Nuevas</option>
-</select>
-<button id="searchBtn">Buscar</button>
+<div class="top-row">
+    <h1> Photosplash</h1>
+    <input type="text" id="searchInput" placeholder="Ejemplo: gato, perro..." />
+</div>
+<div class="bottom-row">
+    <select id="countInput">
+        <option value="12">12</option>
+        <option value="24">24</option>
+        <option value="30">30</option>
+    </select>
+    <select id="orderBy">
+        <option value="relevant">Relevante</option>
+        <option value="latest">Nuevas</option>
+    </select>
+    <button id="searchBtn">Buscar</button>
+</div>
 `;
 
 const Header = () => {
